5098ad1b0aaf932bea1a82e58ce348522c66698b
Steve Yeager <steve.yeager@littlebytegames.com>
2020-11-04 11:46:29 -0800

    Update style for personal tab

--- HelloWorld/src/components/Tab.js
+++ HelloWorld/src/components/Tab.js
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import * as microsoftTeams from "@microsoft/teams-js";
 
 /**
@@ -10,35 +10,75 @@ import * as microsoftTeams from "@microsoft/teams-js";
  * of your app.
  */
 class Tab extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      context: {}
-    }
+      context: {},
+    };
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     // Get the user context from Teams and set it in the state
     microsoftTeams.getContext((context, error) => {
       this.setState({
-        context: context
+        context: context,
       });
     });
     // Next steps: Error handling using the error object
+
+    microsoftTeams.registerOnThemeChangeHandler((theme) => {
+      if (theme !== this.state.theme) {
+        this.setState({ theme });
+      }
+    });
   }
 
   render() {
+    let userName =
+      Object.keys(this.state.context).length > 0
+        ? this.state.context["upn"]
+        : "";
 
-      let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+    const isTheme = this.state.theme;
+    let newTheme;
+    if (isTheme === "default") {
+      newTheme = {
+        backgroundColor: "#EEF1F5",
+        color: "#16233A",
+      };
+    } else {
+      newTheme = {
+        backgroundColor: "#2B2B30",
+        color: "#FFFFFF",
+      };
+    }
 
-      return (
-      <div>
+    return (
+      <div style={newTheme}>
         <h3>Hello World!</h3>
-        <h1>Congratulations {userName}!</h1> <h3>This is the tab you made :-)</h3>
+        <h1>Congratulations {userName}!</h1>{" "}
+        <h3>This is the tab you made :-)</h3>
+        <div>
+          <h1>Important Contacts</h1>
+          <ul>
+            <li>
+              Help Desk:{" "}
+              <a href="mailto:support@company.com">support@company.com</a>
+            </li>
+            <li>
+              Human Resources:{" "}
+              <a href="mailto:hr@company.com">hr@company.com</a>
+            </li>
+            <li>
+              Facilities:{" "}
+              <a href="mailto:facilities@company.com">facilities@company.com</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      );
+    );
   }
 }
 export default Tab;
